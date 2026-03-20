b9f55c3a07b7cfeb20d395c926830deecebe8894
convextriangle <87372947+convextriangle@users.noreply.github.com>
2026-03-20 17:54:37 +0200

    tiny fix for LowLevelWindowVK

--- src/arch/LowLevelWindowVK/LowLevelWindowVK.cpp
+++ src/arch/LowLevelWindowVK/LowLevelWindowVK.cpp
@@ -1,9 +1,15 @@
 #include "LowLevelWindowVK.h"
 #include "Etterna/Globals/global.h"
 #include "arch/arch_default.h"
+#include <cassert>
 
 LowLevelWindowVK*
 LowLevelWindowVK::Create()
 {
+#ifdef _WIN32
+	assert(false && "Should never be called");
+	return nullptr;
+#else
 	return new LOW_LEVEL_WINDOW_VK;
+#endif
 }
