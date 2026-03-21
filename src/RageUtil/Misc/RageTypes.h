f767ef4b8949950eb11fb5488f27705d618ba18e
convextriangle <87372947+convextriangle@users.noreply.github.com>
2026-03-21 16:26:44 +0200

    remove old enums

--- src/RageUtil/Misc/RageTypes.h
+++ src/RageUtil/Misc/RageTypes.h
@@ -645,21 +645,4 @@ struct RageMatrix
 	float m[4][4]{};
 };
 
-enum class RageDisplayType
-{
-	D3D,
-	OGL,
-	Invalid,
-};
-LuaDeclareType(RageDisplayType);
-
-enum class RageShaderType
-{
-	Fragment,
-	Vertex,
-	Compute,
-	Invalid,
-};
-LuaDeclareType(RageShaderType);
-
 #endif
