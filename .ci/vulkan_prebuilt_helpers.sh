000c674d7aa76e59c79d237bad7d19e4d4354116
convextriangle <87372947+convextriangle@users.noreply.github.com>
2026-03-26 22:06:52 +0200

    try to install properly on findows

--- .ci/vulkan_prebuilt_helpers.sh
+++ .ci/vulkan_prebuilt_helpers.sh
@@ -69,7 +69,7 @@ function install_linux() {
 
 function install_windows() {
   test -d $VULKAN_SDK && test -f vulkan_sdk.exe
-  7z x vulkan_sdk.exe -aoa -o$VULKAN_SDK
+  vulkan_sdk.exe in --al --am -c
 }
 
 function install_mac() {
