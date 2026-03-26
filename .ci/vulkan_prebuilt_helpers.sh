7a284d9f6757d988dfb499ac6b58adf3202bcc02
convextriangle <87372947+convextriangle@users.noreply.github.com>
2026-03-26 22:20:51 +0200

    this never ends does it

--- .ci/vulkan_prebuilt_helpers.sh
+++ .ci/vulkan_prebuilt_helpers.sh
@@ -69,7 +69,7 @@ function install_linux() {
 
 function install_windows() {
   test -d $VULKAN_SDK && test -f vulkan_sdk.exe
-  vulkan_sdk.exe in --al --am -c
+  ./vulkan_sdk.exe in --al --am -c
 }
 
 function install_mac() {
