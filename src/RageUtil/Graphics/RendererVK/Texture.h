cdaf0ebfa881b31b53d92d0b732421eb16923afd
convextriangle <87372947+convextriangle@users.noreply.github.com>
2026-04-05 23:30:54 +0300

    texture big

--- src/RageUtil/Graphics/RendererVK/Texture.h
+++ src/RageUtil/Graphics/RendererVK/Texture.h
@@ -12,7 +12,7 @@ struct Texture
 		Wrapping = 0b01,
 		Filtering = 0b10,
 		PossibleSamplerCount = 4,
-		MaxTextures = 1024,
+		MaxTextures = 65536,
 	};
 	VmaAllocation allocation = nullptr;
 	VmaAllocator allocator = nullptr;
